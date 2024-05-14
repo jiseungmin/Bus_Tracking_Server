ce49d9bdcef3a640411ddc1efb509b3e973d2fed
jiseungmin <wltmdals0118@sunmoon.ac.kr>
2024-05-14 21:18:43 +0900

    expo web cors

--- pages/api/notice.js
+++ pages/api/notice.js
@@ -2,6 +2,17 @@ import dbConnect from '../../database/dbconnect';
 import Notice from '../../database/models/notice';
 
 export default async function handler(req, res) {
+  // Set CORS headers
+  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081'); // Allow requests from this origin
+  // res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS'); // Allow these methods
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow these headers
+
+  if (req.method === 'OPTIONS') {
+    // Handle preflight request
+    res.status(200).end();
+    return;
+  }
+
   // DB 연결
   await dbConnect();
   console.log('DB Connect.');
@@ -13,8 +24,10 @@ export default async function handler(req, res) {
     newNotice.content = body;
     await newNotice.save();
     res.status(200).send(newNotice);
-  } else {
+  } else if (req.method === 'GET') {
     const newNotice = await Notice.findById('663884393239381da72e56dc');
     res.status(200).send(newNotice);
+  } else {
+    res.status(405).end(); // Method Not Allowed
   }
 }

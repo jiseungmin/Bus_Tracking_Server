4de2176220294f115190f7e35e2ba7548e6b788f
Jiseungmin <wltmdals0118@sunmoon.ac.kr>
2024-05-19 23:45:02 +0900

    cors busMatricalNumber

--- pages/api/busMatricalNumber.js
+++ pages/api/busMatricalNumber.js
@@ -2,6 +2,10 @@ import dbConnect from '../../database/dbconnect';
 import driverModel from "../../database/models/drivers/M_driver"; // 모델 파일의 경로를 지정하세요
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081'); // Allow requests from this origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS'); // Allow these methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow these headers
+
   // DB 연결
   await dbConnect();
   console.log('DB Connect.');
